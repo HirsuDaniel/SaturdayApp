dfeaf0a7eb254953e832676b0de508e6e1162e56
Daniel Hirsu <daniel.hirsu@hrduo.com>
2022-12-10 16:59:45 +0200

    tasks created

---
+++ Saturday/app/Http/Requests/CreateTaskRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class CreateTaskRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'title' => ['required', 'string', 'max:255'],
+            'description' => ['required', 'string', 'max:500'],
+            'priority' => ['required', 'string', Rule::in(['low', 'medium', 'high'])],
+            'status' => ['required', 'string', Rule::in(['open', 'close', 'in progress', 'blocked', 'testing', 'done'])],
+            'assignee' => ['sometimes', 'int'],
+            'author' => ['required', 'int'],
+        ];
+    }
+}
